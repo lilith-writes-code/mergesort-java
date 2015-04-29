71b8232756640a581a282ed40ce0a4f29057f41d
Jeramie <jeramie.risinger@gmail.com>
2015-04-28 22:23:43 -0700

    Some base cases implemented.

--- src/com/venison/Merger.java
+++ src/com/venison/Merger.java
@@ -2,6 +2,17 @@ package com.venison;
 
 public class Merger {
     public int[] sort(int[] unsorted){
-        return null;
+        if(unsorted == null) {
+            return null;
+        }
+
+        if(unsorted.length == 1){
+            return unsorted;
+        }
+
+        if(unsorted[0] > unsorted[1]){
+            return new int[]{unsorted[1], unsorted[0]};
+        }
+        return unsorted;
     }
 }
--- tests/com/venison/MergerTest.java
+++ tests/com/venison/MergerTest.java
@@ -15,5 +15,36 @@ public class MergerTest {
         assertEquals(null, actual);
     }
 
+    @Test
+    public void sort_SingleItemReturnsSingleItem() throws Exception {
+        Merger testObj = new Merger();
+
+        int[] actual = testObj.sort(new int[]{5});
+
+        assertEquals(5, actual[0]);
+    }
+
+    @Test
+    public void sort_AlreadyTwoItemReturnsSortedSet() throws Exception {
+        Merger testObj = new Merger();
+
+        int[] actual = testObj.sort(new int[]{5, 6});
+
+        assertEquals(5, actual[0]);
+        assertEquals(6, actual[1]);
+    }
+
+    @Test
+    public void sort_unsortedTwoItemReturnsSortedSet() throws Exception {
+        Merger testObj = new Merger();
+
+        int[] actual = testObj.sort(new int[]{6, 5});
+
+        assertEquals(5, actual[0]);
+        assertEquals(6, actual[1]);
+    }
+
+
+
 
 }
