f7aeff546a37c71511c05c2e334215aa18edce70
Jeramie <jeramie.risinger@gmail.com>
2015-05-03 10:44:02 -0700

    Adding a few unit tests.

--- tests/com/venison/MergerTest.java
+++ tests/com/venison/MergerTest.java
@@ -2,6 +2,10 @@ package com.venison;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class MergerTest {
@@ -42,6 +46,45 @@ public class MergerTest {
 
         assertEquals(5, actual[0]);
         assertEquals(6, actual[1]);
+    }
+
+    @Test
+    public void sort_unsortedThreeItemReturnsSortedSet() throws Exception {
+        Merger testObj = new Merger();
+
+        int[] actual = testObj.sort(new int[]{6, 5, 3});
+
+        assertEquals(3, actual[0]);
+        assertEquals(5, actual[1]);
+        assertEquals(6, actual[2]);
+    }
+
+    @Test
+    public void sort_unsortedFourItemReturnsSortedSet() throws Exception {
+        Merger testObj = new Merger();
+
+        int[] actual = testObj.sort(new int[]{6, 5, 3, 9});
+
+        assertEquals(3, actual[0]);
+        assertEquals(5, actual[1]);
+        assertEquals(6, actual[2]);
+        assertEquals(9, actual[3]);
+    }
+
+    @Test
+    public void sort_unsortedManyItemReturnsSortedSet() throws Exception {
+        Merger testObj = new Merger();
+        int[] unsorted = new int[6];
+        Random rando = new Random();
+        for(int i=0; i<unsorted.length; i++){
+            unsorted[i] = rando.nextInt();
+        }
+        int[] copy = Arrays.copyOf(unsorted, unsorted.length);
+        Arrays.sort(copy);
+        int[] actual = testObj.sort(unsorted);
+
+        assertArrayEquals(copy, actual);
+
     }
 
 
